9d3e4001f2f51544a468dfa756f10de37327817a
Devil Lord <46564519+devil9967@users.noreply.github.com>
2019-09-18 19:00:26 +0530

    Add files via upload

---
+++ face_detect.py
@@ -0,0 +1,46 @@
+def run():
+    import cv2
+    import pickle
+    labels={}
+    with open(r'D:\0MY FILES\DEEP@K\CS Project\PYTHON\ML and Data Science\0Projects\Face detection AI\labels.pickle','rb') as f:
+        l=pickle.load(f)
+        labels={v:k for k,v in l.items()}
+    cascPath = r'D:\0MY FILES\DEEP@K\CS Project\PYTHON\ML and Data Science\0Projects\Face detection AI\haarcascade_frontalface_alt2.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    recogniser=cv2.face.LBPHFaceRecognizer_create()
+    recogniser.read(r"D:\0MY FILES\DEEP@K\CS Project\PYTHON\ML and Data Science\0Projects\Face detection AI\trainer.yml")
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+        )
+
+        # Draw a rectangle around the faces
+        # print(faces)
+        for (x, y, w, h) in faces:
+            roi_g=gray[y:y+h,x:x+w]
+            roi=frame[y:y+h,x:x+w]
+            id_,conf=recogniser.predict(roi_g)
+            if conf>=45 and conf<=85:
+                print(labels[id_],conf)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+        #Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+run()
---
+++ face_train.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+from PIL import Image
+import cv2
+import pickle
+
+
+def train():
+    BASE_DIR=os.path.dirname(os.path.abspath(__file__))
+    IMG_DIR=os.path.join(BASE_DIR,'images')
+    cascPath = r'D:\0MY FILES\DEEP@K\CS Project\PYTHON\ML and Data Science\0Projects\Face detection AI\haarcascade_frontalface_alt2.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    recogniser=cv2.face.LBPHFaceRecognizer_create()
+    x_img=[]
+    y_labels=[]
+    label_id={}
+    count=-1
+    for root,dirs,files in os.walk(IMG_DIR):
+        for file in files:
+            if file.endswith('png') or file.endswith('jpeg') or file.endswith('jpg'):
+                path=os.path.join(root,file)
+                label=os.path.basename(root)
+                if label not in label_id:
+                    count += 1
+                    label_id[label]=count
+                img=Image.open(path).convert("L")
+                img_arr=np.array(img,"uint8")
+                face=faceCascade.detectMultiScale(img_arr,scaleFactor=1.5,minNeighbors=5)
+                for x,y,w,h in face:
+                    roi=img_arr[y:y+h,x:x+w]
+                    x_img.append(roi)
+                    print(path)
+                    y_labels.append(count)
+    print(label_id)
+    with open(r"ML and Data Science\0Projects\Face detection AI\labels.pickle",'wb') as f:
+        pickle.dump(label_id,f)
+    recogniser.train(x_img,np.array(y_labels))
+    recogniser.save(r"ML and Data Science\0Projects\Face detection AI\trainer.yml")
+train()
